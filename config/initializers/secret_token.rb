233832e41d4b2f66b6a9083c3da7fc5aa2df4978
Enrique Acevedo <enriquegacevedo@gmail.com>
2013-05-20 11:18:44 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BabyTwitter::Application.config.secret_key_base = 'c5ce28238013b4f87c9398127cefd4330d78537a9677a41f757e3e8159a6b2ea8ff9103b60caa0c220c0210e532959392b160653993f598cc6bb75cfb7d94dd7'
